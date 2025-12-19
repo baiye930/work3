198ee784ce30176f63f91749f3a1cfe0b9897884
baiye930 <3192034028@qq.com>
2025-12-19 19:39:58 +0800

    作业3

---
+++ mastrview.h
@@ -0,0 +1,55 @@
+#ifndef MASTRVIEW_H
+#define MASTRVIEW_H
+
+#include <QWidget>
+#include "loginview.h"
+#include "doctorview.h"
+#include "departmentview.h"
+#include "patienteditview.h"
+#include "patientview.h"
+#include "welcomview.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MastrView;
+}
+QT_END_NAMESPACE
+
+class MastrView : public QWidget
+{
+    Q_OBJECT
+
+public:
+    MastrView(QWidget *parent = nullptr);
+    ~MastrView();
+
+public slots:
+    void goLoginView();
+    void goWelcomView();
+    void goDoctorView();
+    void goDepartmentView();
+    void goPatientEditView(int rowNo);
+    void goPatientView();
+    void goPreviousView();
+
+private slots:
+    void on_stackedWidget_currentChanged(int arg1);
+
+    void on_btBack_clicked();
+
+    void on_btLogout_clicked();
+
+private:
+    void pushWidgetToStackView(QWidget *widget);
+
+    Ui::MastrView *ui;
+
+    WelcomView *welcomeView;
+    DoctorView *doctorView;
+    PatientView *patientView;
+    DepartmentView *departmentView;
+    LoginView *loginView;
+    PatientEditView *patientEditView;
+
+};
+#endif // MASTRVIEW_H
